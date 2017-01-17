237bea301927ef322eb96b3e21cbd98033bb4b22
CRImier <crimier@yandex.ru>
2017-01-17 13:31:10 +0200

    New MAX1704x driver - cleanup and new functions

--- max1704x.py
+++ max1704x.py
@@ -1,12 +1,15 @@
 from smbus import SMBus
-addr = 0x36
 
 class MAX1704x(object):
-    VERSION_LOW_REG = 0x09
+    VERSION_LOW_REG  = 0x09
     VOLTAGE_HIGH_REG = 0x02
-    VOLTAGE_LOW_REG = 0x03
-    SOC_HIGH_REG = 0x04
-    SOC_LOW_REG = 0x05
+    VOLTAGE_LOW_REG  = 0x03
+    SOC_HIGH_REG     = 0x04
+    SOC_LOW_REG      = 0x05
+    CONFIG_HIGH_REG  = 0x0C
+    CONFIG_LOW_REG   = 0x0D
+    CONFIG_ALARM_BIT = 5
+    CONFIG_SLEEP_BIT = 7
 
     voltage_coeffs = {3:1.25, 4:2.5}
     
@@ -23,14 +26,17 @@ class MAX1704x(object):
         return result
 
     def get_soc(self):
-        high = self.bus.read_byte_data(self.addr, self.SOC_HIGH_REG)
-        #low = self.bus.read_byte_data(self.addr, self.SOC_LOW_REG)
-        #result = ((high<<4) + (low>>4)) * self.voltage_coeffs[self.version]
-        result = "{}%".format(high)
+        percents = self.bus.read_byte_data(self.addr, self.SOC_HIGH_REG)
+        result = "{}%".format(percents)
         return result
+
+    def alarm_occured(self):
+        status = self.bus.read_byte_data(self.addr, self.CONFIG_LOW_REG)
+        return status & 1<<self.CONFIG_ALARM_BIT
 
 
 if __name__ == "__main__":
     max1704x = MAX1704x()
     print(max1704x.get_voltage())
     print(max1704x.get_soc())
+    print("Alarm status: {}".format(bool(max1704x.alarm_occured())))
